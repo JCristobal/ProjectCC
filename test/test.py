144e6e489a8973b943d85816efbb839a805c3e1d
J. Cristóbal López <tobas92@gmail.com>
2015-11-23 19:13:29 +0100

    Añado test con la BD

--- test/test.py
+++ test/test.py
@@ -31,8 +31,69 @@ def test_meses():
 	lista = [1,2,3,4,5,6,7,8,9,10,11,12]
 	return lista==meses
 
+# Inserción de un usuario en la base de datos
+def insertarUsuario():
+        post = {"user": "test",
+	    	"nombre": "test_nombre",
+	    	"apellidos": "test_appellido",
+	    	"correo": "test@correo.es",
+	    	"dia": "1",
+	    	"mes": "2",
+	    	"anio": "2001",
+		"direccion": "Calle falsa 123",
+	    	"password": "123456",
+	    	"pago": "VISA",
+	    	"visa": "5555-5555-5555-5555",
+	    	}
 
-# Clase en la que realizamos los test
+
+	posts=db.posts
+	post_id = posts.insert(post) 
+
+	#Comprobamos si realmente se ha insertado en la base de datos
+	query=posts.find({"user": "test"})
+
+	return query.count() != 0
+
+#Edición de un campo del usuario insertado
+def editarUsuario():
+
+   	posts=db.posts
+        posts.update( { "user": "test" }, { '$set': { "nombre": "test_nombre2"}})
+
+	#Comprobamos si realmente se ha actualizado bien la base de datos
+	query=posts.find({"user": "test"})
+
+	return query[0]["nombre"] == "test_nombre2"
+
+#Borrado de un usuario de la base de datos
+def borrarUsuario():
+	
+	posts=db.posts
+	posts.remove({"user" : "test"})
+
+	query=posts.find({"user": "test"})
+
+	return query.count() == 0
+	
+# Clase en la que realizamos los test con la BD
+class test_bd(unittest.TestCase):	
+    def test_insertar(self):
+        print "Insertando en la BD"
+        self.assertTrue(insertarUsuario())
+	
+    def test_editar(self):
+        print "Editando en la BD"
+        insertarUsuario()
+        self.assertTrue(editarUsuario())
+
+    def test_borrar(self):
+        print "Borrando de la BD"
+        insertarUsuario()
+        self.assertTrue(borrarUsuario())
+
+
+# Clase en la que realizamos los test basicos
 class test (unittest.TestCase):
 
     def test_1(self):	
